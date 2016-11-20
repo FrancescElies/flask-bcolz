299f7f175bfa9f88facc4f52ffe01dd3910ba780
Francesc Elies Henar <francesc.elies@gmail.com>
2016-11-20 17:06:08 +0100

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from setuptools import setup
+
+requirements_path = str(Path().resolve() / 'requirements.txt')
+with open(requirements_path) as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='flask_bcolz',
+    install_requires=requirements, )
