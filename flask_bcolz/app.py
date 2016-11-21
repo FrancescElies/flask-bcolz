4507ee4a7514863b25b03b11f42bc7c12a25f4bd
Francesc Elies Henar <francesc.elies@gmail.com>
2016-11-21 10:28:18 +0100

    accepts data sub-subfolders

--- flask_bcolz/app.py
+++ flask_bcolz/app.py
@@ -24,16 +24,24 @@ class MethodResource(six.with_metaclass(MethodResourceMeta, MethodView)):
 
 
 class DataResource(MethodResource):
+    def get_valid_folders(self, data_path):
+        def filter_folders(path):
+            folder_ends_with = ['data', 'meta']
+            return path.parts[-1] not in folder_ends_with
+
+        valid_folders = filter(filter_folders, data_path.glob('**'))
+        return set(valid_folders)
+
     def get(self, folder):
         data_path = Path('data')
-        valid_folders = set(data_path.glob('*'))
+        valid_folders = self.get_valid_folders(data_path)
         if data_path / folder not in valid_folders:
             return 'Invalid folder name', 422
         data = bcolz.open('data/{}'.format(folder))
         return jsonify(list(data))
 
 
-app.add_url_rule('/data/<folder>', view_func=DataResource.as_view('DataResource'))
+app.add_url_rule('/data/<path:folder>', view_func=DataResource.as_view('DataResource'))
 docs.register(DataResource, endpoint='DataResource')
 
 if __name__ == "__main__":
